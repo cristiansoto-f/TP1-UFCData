85de9a10469b881e9ab5c790ddcd49c0a9ebd800
Federico <fedevb94@hotmail.com>
2019-10-23 15:34:06 -0300

    Update TP1.R

--- TP1.R
+++ TP1.R
@@ -1,27 +1,36 @@
 install.packages("data.table")
 library(data.table)
 
-#1. Importar el dataset, guardarlo en un objeto bidimensional (puede ser un data.frame, data.table, tibble, etc.)
-UFCData = fread(file.choose(), fill = T, header = T, sep = ",")
+# 1. Importar el dataset, guardarlo en un objeto bidimensional (puede ser un data.frame, data.table, tibble, etc.)
+dataUFC = fread(file.choose(), fill = T, header = T, sep = ",")
 
-
-#2. Dividir el dataset original en dos datasets distintos, uno con toda la informacion referente al participante
+# 2. Dividir el dataset original en dos datasets distintos, uno con toda la informacion referente al participante
 # de la esquina roja y la informacion en comun del encuentro y otro con la informacion referente al
 # participante de la esquina azul y la informacion en com√∫n del encuentro.
 
-#Extraigo informacion del peleador rojo
-esRedFighter = UFCData[,!grepl( "^B_" , names(UFCData))] #el ^ ayuda a seleccionar solo las columnas que NO EMPIEZAN con B_
+# Extraigo informacion del peleador rojo y comun
+esRedFighter = dataUFC[,!grepl( "^B_" , names(dataUFC))] #el ^ ayuda a seleccionar solo las columnas que NO EMPIEZAN con B_
 
-#Extraigo informacion del peleador azul
-esBlueFighter = UFCData[,!grepl( "^R_" , names(UFCData))]
+# Extraigo informacion del peleador azul y comun
+esBlueFighter = dataUFC[,!grepl( "^R_" , names(dataUFC))]
 
 redFighter <- data.table()
 blueFighter <- data.table()
-for (i in 1:length(esRedFighter)){
+
+for (i in 1:ncol(dataUFC)){
   if (esRedFighter[i]){
-   redFighter <- cbind(redFighter, UFCData[,..i])
+   redFighter <- cbind(redFighter, dataUFC[,..i])
   }
   if (esBlueFighter[i]) {
-    blueFighter <- cbind(blueFighter, UFCData[,..i])
+    blueFighter <- cbind(blueFighter, dataUFC[,..i])
   }
 }
+
+# 3. Para los dos datasets obtenidos en el item anterior
+# a) Reemplazar el pre???jo que indica el color de la esquina en los nombres de las columnas (R_ o B_) por
+# un " (un campo vacÌo). Por ejemplo, el nombre de la columna "R_???ghter" tiene que pasar a llamarse
+# "???ghter". Ambos datasets tendrian que tener nombres de columnas identicos al ???nalizar.
+# b) Crear una variable (o recodear la variable "Winner") que indique si el participante ganÛ la pelea (1) o
+# no (0). Por ejemplo si se est· trabajando con los datos del participante de la esquina azul y la variable
+# Winner toma el valor de Red, entonces la nueva variable tendria que tomar el valor 0.
+# c) Crear una variable que haga referencia al color de la esquina de cada dataset.
