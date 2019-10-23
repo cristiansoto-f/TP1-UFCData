2d233358be85536c5d79656a8887fba98e186463
Federico <fedevb94@hotmail.com>
2019-10-23 15:20:53 -0300

    Update TP1.R

--- TP1.R
+++ TP1.R
@@ -3,20 +3,25 @@ library(data.table)
 
 #1. Importar el dataset, guardarlo en un objeto bidimensional (puede ser un data.frame, data.table, tibble, etc.)
 UFCData = fread(file.choose(), fill = T, header = T, sep = ",")
-UFCData[1:3]
-str(UFCData) #Muestra las distintas variables
 
-#2. Dividir el dataset original en dos datasets distintos, uno con toda la información referente al participante
-# de la esquina roja y la información en común del encuentro y otro con la información referente al
-# participante de la esquina azul y la información en común del encuentro. 
-#Para ejecutar la función grepl es necesario pausar el paquete data.table
+
+#2. Dividir el dataset original en dos datasets distintos, uno con toda la informacion referente al participante
+# de la esquina roja y la informacion en comun del encuentro y otro con la informacion referente al
+# participante de la esquina azul y la informacion en común del encuentro.
+
 #Extraigo informacion del peleador rojo
-RedFighter = UFCData[,grepl( "^R_" , names(UFCData))] #el ^ ayuda a seleccionar solo las columnas que EMPIEZAN con R_
-#Extraigo informacion del peleador azul
-BlueFighter = UFCData[,grepl( "^B_" , names(UFCData))]
-#Extraigo informacion en comun excluyendo mendiante !grepl 
-CommonData = UFCData[,!grepl( "^B_" , names(UFCData)) & !grepl( "^R_" , names(UFCData))]
+esRedFighter = UFCData[,!grepl( "^B_" , names(UFCData))] #el ^ ayuda a seleccionar solo las columnas que EMPIEZAN con R_
 
-#A continuación uno los data table
-RF  = cbind(RedFighter, CommonData)
-BF  = cbind(BlueFighter, CommonData)
+#Extraigo informacion del peleador azul
+esBlueFighter = UFCData[,!grepl( "^R_" , names(UFCData))]
+
+redFighter <- data.table()
+blueFighter <- data.table()
+for (i in 1:length(esRedFighter)){
+  if (esRedFighter[i]){
+   redFighter <- cbind(redFighter, UFCData[,..i])
+  }
+  if (esBlueFighter[i]) {
+    blueFighter <- cbind(blueFighter, UFCData[,..i])
+  }
+}
